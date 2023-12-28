85f95cdecf1095e5a0a7bd6534ac23c07273c8f6
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-12-28 13:25:37 -0700

    modify to use oxen instead of hugging face

--- applications/wikipedia/main.py
+++ applications/wikipedia/main.py
@@ -4,10 +4,11 @@ import asyncio
 import subprocess
 from pathlib import Path
 import time
+import os
 
 from modal import Image, Stub, Volume, gpu, method, Secret
 
-N_GPU = 100
+N_GPU = 10
 GPU_CONFIG = gpu.A10G()
 MODEL_ID = "BAAI/bge-small-en-v1.5"
 MODEL_SLUG = MODEL_ID.split("/")[-1]
@@ -23,9 +24,13 @@ volume = Volume.persisted("embedding-wikipedia")
 cache_dir = "/data"
 data_dir = f"{cache_dir}/{dataset_name}"
 DATA_PATH = Path(data_dir)
+# set OXEN_CONFIG_DIR environment var to data_dir
+os.environ["OXEN_CONFIG_DIR"] = data_dir
 
-PUSH_TO_HUB = False
-dataset_name = f"567-labs/wikipedia-embedding-{MODEL_SLUG}-sample"
+
+PUSH_TO_HUB = True
+# dataset_name = f"567-labs/wikipedia-embedding-{MODEL_SLUG}-sample"
+dataset_name = "oxbot/Wikipedia-Embeddings"
 dataset_file = "wiki-embeddings.parquet"
 
 LAUNCH_FLAGS = [
@@ -145,37 +150,50 @@ class TextEmbeddingsInference:
 
 
 @stub.function(
-    image=Image.debian_slim().pip_install("datasets", "pyarrow", "tqdm"),
+    image=Image.debian_slim()
+        .pip_install("oxenai", "pyarrow", "tqdm", "requests"),
     volumes={cache_dir: volume},
     timeout=84600,
-    secret=Secret.from_name("huggingface-credentials"),
+    secret=Secret.from_name("oxenai-api-key"),
 )
-def embed_dataset(down_scale: float = 0.005, batch_size: int = 512 * 50):
-    from datasets import load_from_disk, load_dataset
+def embed_dataset(
+    # down_scale: float = 0.005,
+    sample_size: int = 5000,
+    batch_size: int = 512 * 50
+):
+    from oxen.streaming_dataset import load_dataset
+    from oxen.auth import config_auth as config_oxen_auth
+    from oxen.remote_repo import create_repo
+
+    import oxen
     import pyarrow as pa
     import pyarrow.parquet as pq
     import time
     import datetime
     import os
+    from tqdm import tqdm
 
     start = time.perf_counter()
-    # Load the dataset as a Hugging Face dataset
-    print("Loading dataset from disk... ~ 40 seconds")
-    dataset = load_from_disk(f"{cache_dir}/wikipedia")
+    # Load the dataset from https://www.oxen.ai/datasets/Wikipedia
+    print("Streaming dataset from Oxen.ai...")
+    # dataset = load_dataset("datasets/Wikipedia", directory="data")
+    dataset = load_dataset("datasets/Wikipedia", paths="data/xaa.arrow")
     print(f"Dataset loaded in {time.perf_counter()-start:.2f} seconds")
 
     # Extract the total size of the dataset
-    ttl_size = len(dataset["train"])
+    ttl_size = len(dataset)
 
     # Counting all characters in the dataset
     dataset_chars = 19560538957  # sum(map(len, dataset["train"]["text"]))
     print(f"Total dataset characters: {dataset_chars}")
 
-    sample_size = int(ttl_size * down_scale)
+    # sample_size = int(ttl_size * down_scale)
     print(f"Calculated dataset size of {ttl_size} and sample size of {sample_size}")
 
     # Iterate over the first 5% of the dataset's rows
-    subset = dataset["train"].select(range(sample_size))
+    subset = []
+    for i in tqdm(range(sample_size)):
+        subset.append(dataset[i])
     model = TextEmbeddingsInference()
 
     print(f"Working with {sample_size} rows")
@@ -199,7 +217,8 @@ def embed_dataset(down_scale: float = 0.005, batch_size: int = 512 * 50):
         datetime.timedelta(seconds=dataset_chars / characters_per_sec)
     )
     resp = {
-        "downscale": down_scale,
+        # "downscale": down_scale,
+        "sample_size": sample_size,
         "batch_size": batch_size,
         "n_gpu": N_GPU,
         "duration_mins": duration / 60,
@@ -209,6 +228,12 @@ def embed_dataset(down_scale: float = 0.005, batch_size: int = 512 * 50):
 
     if PUSH_TO_HUB:
         print(f"Pushing to hub {dataset_name}")
+        oxenai_token = os.environ["OXENAI_API_KEY"]
+        config_oxen_auth(oxenai_token)
+
+        # Initialize the Oxen Repository
+        repo = oxen.init(data_dir)
+        
         table = pa.Table.from_arrays(
             [
                 pa.array([chunk[0] for chunk in acc_chunks]),  # id
@@ -219,17 +244,29 @@ def embed_dataset(down_scale: float = 0.005, batch_size: int = 512 * 50):
             ],
             names=["id", "url", "title", "text", "embedding"],
         )
-        pq.write_table(table, dataset_file)
-        dataset = load_dataset("parquet", data_files=dataset_file)
-        dataset.push_to_hub(dataset_name, token=os.environ["HUGGINGFACE_TOKEN"])
+        pq.write_table(table, os.path.join(data_dir, dataset_file))
+        repo.add(dataset_file)
+        print(repo.status())
+        repo.commit("Adding embeddings")
+
+        remote_repo = create_repo(dataset_name)
+        print("Created remote repo")
+        print(remote_repo)
+        print(remote_repo.url())
+
+        repo.set_remote("origin", remote_repo.url())
+        repo.push()
 
     return resp
 
 
 @stub.local_entrypoint()
 def main():
-    for scale, batch_size in product([0.25], [512 * 50]):
-        with open("benchmarks.json", "a") as f:
-            benchmark = embed_dataset.remote(down_scale=scale, batch_size=batch_size)
-            print(json.dumps(benchmark, indent=2))
-            f.write(json.dumps(benchmark, indent=2) + "\n")
+    # for scale, batch_size in product([0.25], [512 * 50]):
+    #     with open("benchmarks.json", "a") as f:
+    #         benchmark = embed_dataset.remote(down_scale=scale, batch_size=batch_size)
+    #         print(json.dumps(benchmark, indent=2))
+    #         f.write(json.dumps(benchmark, indent=2) + "\n")
+    
+    benchmark = embed_dataset.remote(sample_size=50, batch_size=10)
+    print(json.dumps(benchmark, indent=2))
